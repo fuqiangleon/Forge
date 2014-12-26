94941cb1c19af499e355304fec2a3e446204ddea
Frank Lee <fuqiangleon@gmail.com>
2014-12-26 10:04:50 +0800

    fiexd the black area bug.

--- ECL.py
+++ ECL.py
@@ -12,6 +12,8 @@ import datetime
 
 class MainPanel(wx.Panel):
     def __init__(self, parent):
+        
+        #Fiexd the black area in Screen.pos=(600,0)
         wx.Panel.__init__(self, parent,pos=(600,0))
 
         grid = wx.GridBagSizer(hgap=0, vgap=0)
